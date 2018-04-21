6555c69ad06949894cdce62846f4f6e1448adaac
Aryan Mann <mannarya@grinnell.edu>
2018-04-21 20:22:48 +0000

    Before adding jade

---
+++ app/models/user.js
@@ -0,0 +1,11 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var UserSchema   = new Schema({
+    name: String,
+    email: String, 
+    passwordHash: String,
+    status: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
